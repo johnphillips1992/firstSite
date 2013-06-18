55357c67b1ed6b94a8c06048f5903c38a69ab4e1
John Phillips <johnphillips@arch.tamu.edu>
2013-06-18 10:22:14 -0500

    Added views

--- secondSite/secondSite/books/views.py
+++ secondSite/secondSite/books/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.views.generic import ListView
+from books.models import Publisher
+
+class PublisherList(ListView):
+    model = Publisher
--- secondSite/secondSite/secondSite/urls.py
+++ secondSite/secondSite/secondSite/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from books.views import PublisherList
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'secondSite.views.home', name='home'),
-    # url(r'^secondSite/', include('secondSite.foo.urls')),
-
+    url(r'^publishers/$', PublisherList.as_view()),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
