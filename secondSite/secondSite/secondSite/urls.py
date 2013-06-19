e208947cedad7135348ba4393e8ec3ac5c27a75d
John Phillips <johnphillips@arch.tamu.edu>
2013-06-19 08:46:33 -0500

    Enabled admin

--- secondSite/secondSite/secondSite/urls.py
+++ secondSite/secondSite/secondSite/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from books.views import PublisherList
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^publishers/$', PublisherList.as_view()),
+    url(r'^admin/', include(admin.site.urls)),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+    )
